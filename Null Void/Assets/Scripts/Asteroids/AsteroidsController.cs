b83f9026d5e38482b33a61cefb8818e5578bf6f5
Donnacha Fallon <s00167922@mail.itsligo.ie>
2019-03-23 15:14:30 +0000

    Added asteroids
    still need to get the rotations to work

--- Null Void/Assets/Scripts/Asteroids/AsteroidsController.cs
+++ Null Void/Assets/Scripts/Asteroids/AsteroidsController.cs
@@ -2,15 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AsteroidsController : MonoBehaviour {
+public class AsteroidsController : MonoBehaviour
+{
+    public GameObject Asteroid;
+
+    Transform aTransform;
+
+    [Range(0, 100)]
+    public int numberOfAsteroids;
+    public int AsteroidLimit = 10;
+    
+    public float RotateSpeed;
 
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start ()
+    {
+        aTransform = Asteroid.GetComponent<Transform>();
+        
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        aTransform.Rotate(0, 6 * Time.deltaTime, 0);
+        if (numberOfAsteroids <= AsteroidLimit)
+        {
+            Instantiate(Asteroid, new Vector3(Random.Range(-1000, 1000), 0, Random.Range(-1000, 1000)), Quaternion.identity);
+            
+            
+            numberOfAsteroids++;
+        }
+        
 	}
 }
