89c1e1a9ef752dcf07ede2e31274a68d38ae5252
Jack Gilmartin <s00171013@mail.itsligo.ie>
2019-03-24 20:39:13 +0000

    Registration feedback confirmed to  appear in-game.

--- Null Void/Assets/Scripts/Server/ServerListener.cs
+++ Null Void/Assets/Scripts/Server/ServerListener.cs
@@ -2,13 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ServerListener : MonoBehaviour // This script is solely responsible for obtaining information from the server.
 {
     public static ServerListener instance;
     public bool LoggedIn;
+    
 
-    SignalRController signalRController;    
+    SignalRController signalRController;
+    UiController uiController;
 
     void Awake()
     {
@@ -24,9 +27,9 @@ public class ServerListener : MonoBehaviour // This script is solely responsible
     }
 
     void Start()
-    {       
-        // Find the signalR controller.
-        signalRController = GameObject.Find("SignalRController").GetComponent<SignalRController>();               
+    {              
+        signalRController = GameObject.Find("SignalRController").GetComponent<SignalRController>();
+        uiController = GameObject.Find("Controller_Menu").GetComponent<UiController>();
     }
 
     void Update()
@@ -72,17 +75,19 @@ public class ServerListener : MonoBehaviour // This script is solely responsible
     {
         if (status.ToUpper() == "EMAIL TAKEN")
         {
-            print(input + " already has a NULL VOID account attached to it!"); // This should be replaced by in-game text.
+            uiController.UpdateRegFeedback(input + " already has a NULL VOID account attached to it!");                      
         }
 
         else if (status.ToUpper() == "USERNAME TAKEN")
         {
-            print("The username " + "'"+input+"'" + " has already been taken."); // This should also be replaced.
+            //uiController.UpdateRegFeedback();
+            //print("The username " + "'"+input+"'" + " has already been taken."); // This should also be replaced.
         }
 
         else if (status.ToUpper() == "SUCCESS")
         {
-            print("Welcome to NULL VOID, " + input + "! Please log in so you can play the game!");
+            //uiController.UpdateRegFeedback();
+            //print("Welcome to NULL VOID, " + input + "! Please log in so you can play the game!");
         }
     }
 
--- Null Void/Assets/Scripts/UI/UiController.cs
+++ Null Void/Assets/Scripts/UI/UiController.cs
@@ -8,10 +8,15 @@ using UnityEngine.UI;
 public class UiController : MonoBehaviour
 {
     public static UiController instance;
-    public Text txtEmail, txtUsernameR, txtPasswordR, txtUsernameL, txtPasswordL;
+    public Text txtEmail, txtUsernameR, txtPasswordR, txtUsernameL, txtPasswordL; // For input fields.
+    public Text RegistrationFeedback, LoginFeedback, UsernameDisplay, WinLossDisplay; // To display in-game.
+    public UnityEngine.UI.Button ReturnFromRegistration, ReturnFromLogin;
+
+    string serverMessage = "";
+    bool feedbackUpdated;
 
     SignalRController signalRController;
-    public AudioManager audioManager;
+    AudioManager audioManager;
 
     void Awake()
     {
@@ -30,16 +35,42 @@ public class UiController : MonoBehaviour
     {
         signalRController = GameObject.Find("SignalRController").GetComponent<SignalRController>();
         audioManager = GameObject.Find("Controller_Audio").GetComponent<AudioManager>();
-        PlaySound("Menu BGM"); // Play the main menu BGM.       
+        PlaySound("Menu BGM"); // Play the main menu BGM.      
+
+        RegistrationFeedback.text = "Registering...";
     }
 
-    public void SendRegisterInfo()
+    private void Update()
+    {
+        if(feedbackUpdated)
+        {
+            RegistrationFeedback.text = serverMessage;
+            ReturnFromRegistration.gameObject.SetActive(true); // I'll keep an eye on this.
+            feedbackUpdated = false;
+        }
+    }
+
+    #region Registration methods and coroutines.
+    public void CheckRegisterInfo()
     {
         if(txtEmail != null && txtUsernameR != null && txtPasswordR != null)
         {
-            signalRController.RegisterPlayer(txtEmail.text, txtUsernameR.text, txtPasswordR.text);
+            StartCoroutine("SendRegisterInfo");
         }
     }
+
+    public IEnumerator SendRegisterInfo()
+    {
+        yield return new WaitForSeconds(2);
+        signalRController.RegisterPlayer(txtEmail.text, txtUsernameR.text, txtPasswordR.text);
+    }
+
+    public void UpdateRegFeedback(string serverFeedback)
+    {
+        serverMessage = serverFeedback;
+        feedbackUpdated = true;
+    }
+    #endregion
 
     public void SendLoginInfo()
     {
@@ -51,19 +82,25 @@ public class UiController : MonoBehaviour
 
     public void StartGame()
     {
-        StopSound("Menu BGM");
-        PlaySound("Confirm");
+        StopSound("Menu BGM");       
         PlaySound("Gameplay BGM"); // Play the gameplay BGM.
         SceneManager.LoadScene("sc_gameplay");
     }
 
     public void QuitGame()
-    {
-        PlaySound("Confirm");
+    {       
         Application.Quit(); // Close the game.       
     }
+
+    public void RevertFeedback()
+    {
+        if (RegistrationFeedback.text != "Registering...") RegistrationFeedback.text = "Registering...";
+        //if (LoginFeedback.text != "Logging in...") LoginFeedback.text = "Logging in...";
+
+        ReturnFromRegistration.gameObject.SetActive(false);
+    }
    
-    public void PlaySound(string soundName)
+    public void PlaySound(string soundName) // I should probably move these next two methods to the AudioManager class.
     {
         if(audioManager != null) audioManager.Play(soundName);
     }
